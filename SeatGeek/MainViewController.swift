c20a873d6c0b1926bcf59ccbd9eba421bb323e57
Eric Ngo <ericn441@gmail.com>
2017-05-30 17:09:40 -0500

    Initial commit

---
+++ SeatGeek/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  SeatGeek
+//
+//  Created by Eric on 5/30/17.
+//  Copyright © 2017 Eric. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
